06bc46024bff809d923d1dff8386f760d2b04657
shahbaz ali <shahbaz_ali@outlook.in>
2021-10-11 00:20:15 +0530

    connect mongoDB

--- app.js
+++ app.js
@@ -1,35 +1,57 @@
-const express = require('express');
-const ejs = require('ejs');
+const express = require("express");
+const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const https = require("https");
-require("dotenv").config() ;  
+require("dotenv").config();
+const mongoose = require("mongoose"); //requiring mongoose
 
 app = express();
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true})); 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+const DB = "mongodb+srv://"+process.env.mongo_Username+":"+process.env.mongo_Password+"@cluster0.2lld6.mongodb.net/attendanceDB?retryWrites=true&w=majority";
 
-list = [{name: "Monkey", present: "False", rollNo : "1" }, {name: "Dog", present: "False", rollNo : "2" }, {name: "Cow", present: "False" , rollNo : "3"}, {name: "Raptor", present: "False" , rollNo : "4" }];
+mongoose.connect(DB).then(()=> {
+  console.log("Connection Sucessful");
+}).catch((err)=>console.log("No Connection"));
 
+list = [
+  { name: "Monkey", present: "False", rollNo: "1" },
+  { name: "Dog", present: "False", rollNo: "2" },
+  { name: "Cow", present: "False", rollNo: "3" },
+  { name: "Raptor", present: "False", rollNo: "4" },
+];
 
-app.get("/", function(req, res){
-    res.render("home");
+const itemsSchema = {    // schema for items
+  name : String
+};
+
+const Item  =  mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name : "Welcome to your to do list"
 })
 
-app.post("/", function(req, res){
-    res.send("Hey "+req.body.name + "! Your attendance has been marked!")
-    const url = process.env.URL + "?p1="+req.body.name+"&p2=Present"
-    https.get(url, function(){
-        console.log("Sent Request");
-    })
-})
+item1.save();
 
-app.get("/a", function(req, res){
-    res.render("a", {list: list });
-})
+app.get("/", function (req, res) {
+  res.render("home");
+});
 
-app.listen(8000, function(){
-    console.log("Server is running on port 8000");
+app.post("/", function (req, res) {
+  res.send("Hey " + req.body.name + "! Your attendance has been marked!");
+  const url = process.env.URL + "?p1=" + req.body.name + "&p2=Present";
+  https.get(url, function () {
+    console.log("Sent Request");
   });
+});
+
+app.get("/a", function (req, res) {
+  res.render("a", { list: list });
+});
+
+app.listen(8000, function () {
+  console.log("Server is running on port 8000");
+});
